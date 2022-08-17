81fff0d4a5ed319359163c9397301d71a43fdf8a
devfelixarthur <de.felixarthur@gmail.com>
2022-08-17 18:53:44 -0300

    TRABALHO CONDICIONAIS I

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,25 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let nome = prompt('Qual o seu nome?')
 
-if(){
-
+let idade = +prompt('Quantos anos você tem ?')
+if(idade >=18){
+    console.log(`${nome} é maior de idade.`)
 }else{
-	
+    console.log(`${nome} é menor de idade.`)
 }
+
+let ensinoMedio = prompt ('Você terminou o ensino médio?')
+if(ensinoMedio === 'sim'){
+    ensinoMedio = true
+}else{
+    ensinoMedio = false
+}
+
+let cursaFaculdade = prompt ('Você está cursando alguma graduação?')
+if( cursaFaculdade === 'sim'){
+    cursaFaculdade = true
+}else{
+    cursaFaculdade = false
+}
+
+console.log (`${nome} terminou o ensino médio? ${ensinoMedio}.`)
+console.log (`${nome} está cursando alguma graduação? ${cursaFaculdade}.`)
